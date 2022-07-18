adccd2f5975d5d47baee21cddcd81be7a091eb4a
prats234 <109065276+prats234@users.noreply.github.com>
2022-07-18 20:28:09 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,17 @@
+public class datatypes {
+    public static void main (String[] args)
+    {
+        int a = 34;
+        double b = 354.543;
+        float c = 2.4f;
+        boolean d = true;
+        String e = "Sid";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
